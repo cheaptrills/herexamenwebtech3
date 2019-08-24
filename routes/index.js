ca91dd873c394af0b335b67dbdda9e650fb50c19
Vince van alphen <vincevanalphen@yahoo.com>
2019-08-24 23:02:41 +0200

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Express' });
+});
 module.exports = router;
