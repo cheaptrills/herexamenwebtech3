500b088b94d28b783ac2f21a33fe1856f35a5bcb
Vince van alphen <vincevanalphen@yahoo.com>
2019-08-25 22:23:27 +0200

    added passport.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const apiChatRouter = require('./routes/api/v1/chat');
+const passport = require('./passport/passport');
 
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
@@ -28,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/chat', apiChatRouter);
+app.use('/api/v1/chat',passport.authenticate('jwt', { session: false }), apiChatRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- passport/passport.js
+++ passport/passport.js
@@ -7,3 +7,26 @@ passport.use(User.createStrategy());
 // use static serialize and deserialize of model for passport session support
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secretje';
+/*opts.issuer = 'accounts.examplesoft.com';
+opts.audience = 'yoursite.net';*/
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({_id: jwt_payload.uid}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
+
+module.exports = passport;
