cbb389180c191a247bda5fe0d80ab20999112efd
Vince van alphen <vincevanalphen@yahoo.com>
2019-08-25 22:25:00 +0200

    added chat js

---
+++ public/javascript/chat.js
@@ -0,0 +1,31 @@
+var btnLogin = document.querySelector('#submit').addEventListener("click",(e)=>{
+    let message = document.querySelector('#chat').value;
+
+    fetch('http://localhost:3000/api/v1/chat',{
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+            },
+            body: JSON.stringify({
+
+                "message": message
+            })
+        }).then(response =>{
+                return response.json();
+        })
+});
+
+const getMessages = async () => {
+    const result = await fetch('http://localhost:3000/api/v1/chat',{
+        method: "get",
+        headers:{
+            'Authorization': 'Bearer ' + localStorage.getItem("token")
+        }
+    });
+
+    if(result.status === 200){
+        data = await result.json();
+        
+    }
+};
